0979dc2ee5d823db7c3a1a1757186e4b1cb6750f
Sam Esserman <sesserman97@gmail.com>
2023-05-10 14:16:39 -0400

    still having issues

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ let deck = [];
 
 let canHit = true
 
-window.onload = function() {
+window.addEventListener('load', () => {
   buildDeck();
   shuffleDeck();
   startGame();
-}
+});
+
 
 const buildDeck = () => {
   let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
